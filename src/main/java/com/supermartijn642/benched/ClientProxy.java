f6a19e1066e7f28ae275481f75cbbc2423a6bbfe
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-11 00:06:13 +0100

    update version

--- src/main/java/com/supermartijn642/benched/ClientProxy.java
+++ src/main/java/com/supermartijn642/benched/ClientProxy.java
@@ -3,7 +3,6 @@ package com.supermartijn642.benched;
 import com.supermartijn642.benched.seat.SeatEntityRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;

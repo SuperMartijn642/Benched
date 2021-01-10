a4f50319770b8d361ffa82a1a3e20fcb46fa5a70
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-10 23:51:01 +0100

    remove the tile renderer lower the texture size

--- src/main/java/com/supermartijn642/benched/ClientProxy.java
+++ src/main/java/com/supermartijn642/benched/ClientProxy.java
@@ -1,14 +1,8 @@
 package com.supermartijn642.benched;
 
-import com.supermartijn642.benched.blocks.BenchRenderer;
 import com.supermartijn642.benched.seat.SeatEntityRenderer;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -22,7 +16,5 @@ public class ClientProxy {
     @SubscribeEvent
     public static void onSetup(FMLClientSetupEvent e){
         RenderingRegistry.registerEntityRenderingHandler(Benched.seat_entity, SeatEntityRenderer::new);
-
-        ClientRegistry.bindTileEntityRenderer(Benched.bench_tile, BenchRenderer::new);
     }
 }
